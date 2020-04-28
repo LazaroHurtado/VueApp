dafdfa16d28829ef3692bf14c6c645cfef3b60d4
lazarohurtado <hurtadolazaro12@gmail.com>
2020-04-28 10:29:05 -0400

    header and login page

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <header>
+            <router-link to="/" id="Name">Kelp</router-link>
+        </header>
+        <nav>
+            <router-link to="/login">Login</router-link>
+            <router-link to="/sign-up">Sign Up</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .container {
+        background: #293250;
+        border-bottom: 5px solid #6DD47E;
+        overflow: hidden;
+        width: 100%;
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    nav {
+        display: flex;
+        align-items: center;
+    }
+
+    header {
+        display: flex;
+        align-items: center;
+    }
+
+    header a{
+        font-family: 'Montserrat-BlackItalic', sans-serif;
+        color: #fff;
+        margin: 0px;
+        float: left;
+        text-align: center;
+        padding-left: 10px;
+        text-decoration: none;
+        font-size: 30px;
+    }
+
+    nav a {
+        padding: 10px 10px;
+        color: #fff;
+        text-align: center;
+        text-decoration: none;
+    }
+
+    @font-face {
+        font-family: 'Montserrat-BlackItalic';
+        src: url('../../assets/fonts/Montserrat-BlackItalic.ttf');
+    }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <form>
+        <p>Username</p>
+        <input type="text" name="" placeholder="John Doe">
+        <p>Password</p>
+        <input type="password" name="" placeholder="******">
+        <input type="submit" name="" value="Login">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+    div {
+        background: #FFD55A;
+        width: 500px;
+        height: 350px;
+        border: 5px solid #6DD47E;
+        border-radius: 10px;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 50px;
+    }
+
+    form input[type="text"], input[type="password"] {
+        width: 50%;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    form p {
+        color: #293250;
+        font-family: 'Montserrat-SemiBold', sans-serif;
+    }
+
+    form input[type="text"], input[type="password"] {
+        border: none;
+        border-bottom: 1px solid #293250;
+        background: transparent;
+        outline: none;
+    }
+
+    form input[type="submit"]{
+        margin-top: 20px;
+        border: none;
+        outline: none;
+        height: 30px;
+        background: #293250;
+        color: #fff;
+        font-size: 15px;
+        border-radius: 5px;
+    }
+
+    h1 {
+        color: #293250;
+        font-family: 'Montserrat-SemiBold', sans-serif;
+        font-weight: 1600px;
+    }
+
+    @font-face {
+        font-family: 'Montserrat-SemiBold';
+        src: url('../assets/fonts/Montserrat-SemiBold.ttf');
+    }
+</style>
